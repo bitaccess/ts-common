2ba2d22f580523415a7a96d4125d3f1ab5fe017f
Dylan Seago <dylan@bitaccess.co>
2019-10-29 17:00:47 -0400

    Add toBigNumber helper

---
+++ src/utils/helpers.ts
@@ -0,0 +1,17 @@
+import BigNumber from 'bignumber.js'
+import { Numeric } from '#/types'
+import { isNil } from '#/guards'
+
+export function toBigNumber(value: Numeric): BigNumber
+export function toBigNumber(value: Numeric | null): BigNumber | null
+export function toBigNumber(value: Numeric | undefined): BigNumber | undefined
+export function toBigNumber(value: Numeric | null | undefined): BigNumber | null | undefined
+export function toBigNumber(value: Numeric | null | undefined): BigNumber | null | undefined {
+  if (isNil(value)) {
+    return value
+  }
+  if (value instanceof BigNumber) {
+    return value
+  }
+  return new BigNumber(value)
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './validation'
 export * from './string'
+export * from './helpers'
