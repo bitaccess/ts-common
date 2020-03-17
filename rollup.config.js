c025dc73eeb2b75fe6e46b23b07155ff480cdfe7
Dylan Seago <dylan@bitaccess.co>
2020-03-17 19:27:54 -0400

    Don't include fp-ts in build artifacts

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
 const base = require('@faast/ts-config/library/rollup.config')
 const pkg = require('./package.json')
 
+const baseConfig = base(pkg)
+
+const externalModuleRegexp = new RegExp(__dirname + '/node_modules/([\\w-_]+)/.*')
+
 export default {
-  ...base(pkg),
-  // overrides here
+  ...baseConfig,
+  external: [
+    ...baseConfig.external,
+    // Don't want these in build artifacts
+    'fp-ts/lib/Record',
+    'io-ts/lib/Reporter',
+  ]
 }
