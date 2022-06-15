a00433475ddd74bdb6861d65ff61d5c7cd1ad20c
dylanseago <dylan@bitaccess.co>
2022-06-15 13:36:59 -0700

    Remove broken typedoc

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ROOT="`cd $(dirname $0)/..; echo $PWD`"
-"$ROOT/node_modules/@faast/ts-config/library/bin/build.sh" "$ROOT"
+NO_DOCS=1 "$ROOT/node_modules/@faast/ts-config/library/bin/build.sh" "$ROOT"
--- bin/docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ROOT="`cd $(dirname $0)/..; echo $PWD`"
-"$ROOT/node_modules/@faast/ts-config/library/bin/docs.sh" "$ROOT"
